65e0030aad58f75db0ac2713d0b1c6dfb6098647
watchakorn-18k <porton2559@gmail.com>
2022-05-07 21:25:54 +0700

    บทที่ 1

---
+++ บทที่ 1/app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/word")
+def word():
+    return render_template("word.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,port=10100,host="0.0.0.0",use_reloader=True)
